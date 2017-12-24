c26f7b997cdb14d3fae1591b90f148f36e60a0d8
Nikhil Singh <lenikhilsingh@gmail.com>
2017-12-25 03:35:39 +0530

    Add files via upload

---
+++ Signin Screen2/src/sample/Controller.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import java.io.File;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static java.util.Collections.replaceAll;
+
+public class Controller implements Initializable {
+    @FXML
+    private MediaView mediaView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        Media media = new Media(new File("C:\\Users\\Nikhil\\IdeaProjects\\Signin Screen2\\src\\sample\\video.mp4").toURI().toString());
+        MediaPlayer player= new MediaPlayer(media);
+        mediaView.setMediaPlayer(player);
+        player.play();
+        player.setVolume(0);
+
+
+    }
+}
---
+++ Signin Screen2/src/sample/Main.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    Stage  stage;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+       this.stage= primaryStage;
+       mainWindow();
+    }
+
+    public void mainWindow(){
+
+        try {
+            FXMLLoader loader=new FXMLLoader(Main.class.getResource("sample.fxml"));
+            AnchorPane pane= loader.load();
+
+            Scene scene= new Scene(pane);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
